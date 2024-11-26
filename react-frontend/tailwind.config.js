644cd8d304d228d90ededa4d31c2e0c5d54b6905
SandunSandeepa <sandunsandeepa567@gmail.com>
2024-11-26 12:55:44 +0530

    Front-end setup

---
+++ react-frontend/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+      "./src/**/*.{js,jsx,ts,tsx}", // Scan these files for classes
+  ],
+  theme: {
+      extend: {}, // You can customize Tailwind here if needed
+  },
+  plugins: [],
+};
